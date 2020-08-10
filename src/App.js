a4ba705321e5d23dc51bcc9ab20c344e18313103
amer519 <afahmy519@gmail.com>
2020-08-10 15:08:48 -0400

    image upload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { db, auth } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import { Button, Input } from '@material-ui/core';
+import ImageUpload from './ImageUpload';
 
 function getModalStyle() {
   const top = 50;
@@ -94,6 +95,13 @@ function App() {
 
   return (
     <div className="app">
+
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName} />
+      ): (
+        <h3>Sorry you need to be logged in to upload</h3>
+      )}
+      
 
         <Modal
         open={open}
